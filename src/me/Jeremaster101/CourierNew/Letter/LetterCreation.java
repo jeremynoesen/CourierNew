1798b83644e09495b2346208b54d83ca3e70c0be
Nicholas Xavier <nicho@nicho.dev>
2020-06-20 07:29:08 -0700

    fix command running exploit

--- src/me/Jeremaster101/CourierNew/Letter/LetterCreation.java
+++ src/me/Jeremaster101/CourierNew/Letter/LetterCreation.java
@@ -72,6 +72,9 @@ public class LetterCreation {
         String plainMessage = msg.unformat(message);
         
         String wrapped = WordUtils.wrap(plainMessage, 30, "<split>", true);
+        if (wrapped.startsWith("/")) {
+            wrapped = wrapped.substring(1);
+        }
         String[] lines = wrapped.split("<split>");
         
         ArrayList<String> lore = new ArrayList<>();

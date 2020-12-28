5bb35ee89c1a5d5fc01a354c463869dd836d492a
Jeremy Noesen <jeremynoesen@gmail.com>
2020-12-28 16:07:44 -0600

    fixed error spam caused by clicking any entity

--- src/jeremynoesen/couriernew/letter/LetterSender.java
+++ src/jeremynoesen/couriernew/letter/LetterSender.java
@@ -193,20 +193,21 @@ public class LetterSender implements Listener {
     @EventHandler
     public void onEntityInteract(PlayerInteractEntityEvent e) {
         Entity en = e.getRightClicked();
-        if ((Courier.getCouriers().containsKey(en) && !CourierOptions.PROTECTED_COURIER ||
-                Courier.getCouriers().get(en).getRecipient().equals(e.getPlayer()) &&
-                        !Courier.getCouriers().get(en).isDelivered())) {
-            Courier.getCouriers().get(en).setDelivered();
-            e.setCancelled(true);
-            receive(e.getPlayer());
-            en.getWorld().playSound(en.getLocation(), Sound.BLOCK_WOOL_BREAK, 1, 1);
-            en.getWorld().spawnParticle(Particle.VILLAGER_HAPPY,
-                    en.getLocation().add(0, en.getHeight() / 2, 0), 20,
-                    en.getWidth() / 2, en.getHeight(), en.getWidth() / 2);
-        } else if (!Courier.getCouriers().get(en).getRecipient().equals(e.getPlayer())) {
-            e.setCancelled(true);
-            en.getWorld().playSound(en.getLocation(), Sound.UI_TOAST_OUT, 1, 1);
-        } else if (Courier.getCouriers().get(en).isDelivered()) e.setCancelled(true);
+        if (Courier.getCouriers().containsKey(en)) {
+            if (!CourierOptions.PROTECTED_COURIER || (Courier.getCouriers().get(en).getRecipient().equals(e.getPlayer())
+                    && !Courier.getCouriers().get(en).isDelivered())) {
+                Courier.getCouriers().get(en).setDelivered();
+                e.setCancelled(true);
+                receive(e.getPlayer());
+                en.getWorld().playSound(en.getLocation(), Sound.BLOCK_WOOL_BREAK, 1, 1);
+                en.getWorld().spawnParticle(Particle.VILLAGER_HAPPY,
+                        en.getLocation().add(0, en.getHeight() / 2, 0), 20,
+                        en.getWidth() / 2, en.getHeight() / 2, en.getWidth() / 2);
+            } else if (!Courier.getCouriers().get(en).getRecipient().equals(e.getPlayer())) {
+                e.setCancelled(true);
+                en.getWorld().playSound(en.getLocation(), Sound.UI_TOAST_OUT, 1, 1);
+            } else if (Courier.getCouriers().get(en).isDelivered()) e.setCancelled(true);
+        }
     }
     
     /**
